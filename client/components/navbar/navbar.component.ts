a027b40c241a3c47494c9f80c14cc4db058abf22
Jean Philip de Rogatis <jrogatis@iMac-de-Jean.local>
2016-11-16 15:23:10 -0200

    nav settings

--- client/components/navbar/navbar.component.ts
+++ client/components/navbar/navbar.component.ts
@@ -10,38 +10,48 @@ export class NavbarComponent {
     'link': '/'
   }, {
     'title': 'CAUSAS ',
-    'link': '/'
+    'link': 'causas'
   }, {
     'title': 'METODOLOGIA ',
-    'link': '/'
+    'link': 'metodologia'
   }, {
     'title': 'SOBRE ',
-    'link': '/'
+    'link': 'sobre'
   }, {
     'title': 'BLOG ',
     'link': '/'
   }, {
     'title': 'CONTATO ',
-    'link': '/'
+    'link': 'contato'
   }];
 
   $location;
+  anchorSmoothScroll;
   isLoggedIn: Function;
   isAdmin: Function;
   getCurrentUser: Function;
   isCollapsed = true;
 
-  constructor($location, Auth) {
+  constructor($location, Auth,  anchorSmoothScroll) {
     'ngInject';
     this.$location = $location;
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
+    this.anchorSmoothScroll = anchorSmoothScroll;
   }
 
   isActive(route) {
     return route === this.$location.path();
   }
+
+  goToElement(eID) {
+    console.log(eID);
+    this.$location.hash(eID);
+    this.anchorSmoothScroll.scrollTo(eID)
+  }
+
+   
 }
 
 export default angular.module('directives.navbar', [ngMaterial])
@@ -49,5 +59,58 @@ export default angular.module('directives.navbar', [ngMaterial])
     template: require('./navbar.pug'),
     controller: NavbarComponent
   })
+  .service('anchorSmoothScroll', function(){
+    
+    this.scrollTo = function(eID) {
+
+        // This scrolling function 
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+        
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+        
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop)
+                return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+        
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            } return y;
+        }
+
+    };
+    
+})
   .name;
 
