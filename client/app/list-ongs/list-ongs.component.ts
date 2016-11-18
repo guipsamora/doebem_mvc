68a8acbed38a99e6adb5cbb868c1fb015a3e5989
Jean Philip de Rogatis <jrogatis@iMac-de-Jean.local>
2016-11-18 08:51:36 -0200

    listOng filtros funcionando

--- client/app/list-ongs/list-ongs.component.ts
+++ client/app/list-ongs/list-ongs.component.ts
@@ -9,16 +9,20 @@ export class ListOngsController {
   stepOptions = [];
   listOngFilters = [];
   listOng = [];
+  pageTitle;
+  pageImage;
 
   /*@ngInject*/
   constructor($http, $scope, socket, $routeParams) {
     this.$http = $http;
     this.$scope = $scope;
     this.$routeParams =  $routeParams;
+    this.pageTitle;
+    this.pageImage;
 
     
     this.listOng= [
-      {nome: 'Exemplo 1', AreaDeAtuação: 'educação' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'},
+      {nome: 'Exemplo 1', AreaDeAtuação: 'Educação' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'},
       {nome: 'Exemplo 2', AreaDeAtuação: 'Saude' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'},
       {nome: 'Exemplo 3', AreaDeAtuação: 'Combate á Pobreza' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'} 
     ]
@@ -28,21 +32,39 @@ export class ListOngsController {
       title: 'Educação',
       image: './assets/images/educacao/lousa_edu3.jpg'
       },
-      {title: 'Combate á Pobreza',
-      image: './../../assets/images/combate_pobreza/1.png'
+      {title: 'CombateAPobreza',
+      image: './assets/images/combate_pobreza/1.png'
       },
        {title: 'Saude',
-      image: './../../assets/images/saude/2.png'
+      image: './assets/images/saude/2.png'
       }
       ]   
   }
 
   $onInit() {
     console.log('rota',  this.$routeParams.filterCausa)
+    this.setPageFilter();
   }
 
   setPageFilter() {
-    
+    switch(this.$routeParams.filterCausa) {
+      case 'Saude':
+        this.pageTitle = 'Saúde';
+        this.pageImage = './assets/images/saude/2.png';
+        break;
+      case 'CombateAProbreza':
+        this.pageTitle = 'Combate a Pobreza';
+        this.pageImage = './assets/images/combate_pobreza/1.png';
+        break;
+      case 'Educação':
+        this.pageTitle = 'Educação';
+        this.pageImage = './assets/images/educacao/lousa_edu3.jpg';
+        break;
+      default:
+      this.pageTitle = 'Ongs que apoiamos';
+      this.pageImage = './assets/images/educacao/lousa_edu3.jpg';
+
+    }
   }
 
 
