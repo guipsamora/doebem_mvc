477943c1b64d8ec50233fc44828f5054b1dc1da9
Jean Philip de Rogatis <jrogatis@iMac-de-Jean.local>
2016-11-18 13:28:36 -0200

    ajustes lista ong

--- client/app/list-ongs/list-ongs.component.ts
+++ client/app/list-ongs/list-ongs.component.ts
@@ -1,80 +1,92 @@
-const angular = require('angular');
-const ngRoute = require('angular-route');
+const angular=require('angular');
+const ngRoute=require('angular-route');
 import routing from './list-ongs.routes';
-
 export class ListOngsController {
   $http;
   $scope;
   $routeParams;
-  stepOptions = [];
-  listOngFilters = [];
-  listOng = [];
+  stepOptions=[];
+  listOngFilters=[];
+  listOng=[];
   pageTitle;
   pageImage;
-
+  listOngToDisplay = [];
   /*@ngInject*/
   constructor($http, $scope, socket, $routeParams) {
-    this.$http = $http;
-    this.$scope = $scope;
-    this.$routeParams =  $routeParams;
+    this.$http=$http;
+    this.$scope=$scope;
+    this.$routeParams=$routeParams;
     this.pageTitle;
     this.pageImage;
+    this.listOngToDisplay;
 
-    
-    this.listOng= [
-      {nome: 'Exemplo 1', AreaDeAtuação: 'Educação' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'},
-      {nome: 'Exemplo 2', AreaDeAtuação: 'Saude' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'},
-      {nome: 'Exemplo 3', AreaDeAtuação: 'Combate á Pobreza' , desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png'} 
-    ]
+    this.listOng=[ {
+      nome: 'Exemplo 1', areaDeAtuacao: 'Educacao', desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png', localidades: 'Rio de Janeiro'
+    }
+    ,
+    {
+      nome: 'Exemplo 2', areaDeAtuacao: 'Saude', desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png', localidades: 'São Paulo'
+    }
+    ,
+    {
+      nome: 'Exemplo 3', areaDeAtuacao: 'CombateAPobreza', desc: 'Bla Bla Bla Bla Bla Bla', logo: '../../assets/images/educacao/gauss/logo-gauss.png', localidades: 'Rio de Janeiro - São Paulo'
+    }
+    ]; 
 
+    this.listOngFilters=[{
+      title: 'Educação', image: './assets/images/educacao/lousa_edu3.jpg'
+    }
+    ,
+    {
+      title: 'CombateAPobreza', image: './assets/images/combate_pobreza/1.png'
+    }
+    ,
+    {
+      title: 'Saude', image: './assets/images/saude/2.png'
+    }
+    ];
 
-    this.listOngFilters = [{
-      title: 'Educação',
-      image: './assets/images/educacao/lousa_edu3.jpg'
-      },
-      {title: 'CombateAPobreza',
-      image: './assets/images/combate_pobreza/1.png'
-      },
-       {title: 'Saude',
-      image: './assets/images/saude/2.png'
-      }
-      ]   
   }
 
   $onInit() {
-    console.log('rota',  this.$routeParams.filterCausa)
-    this.setPageFilter();
+     this.setPageFilter()
   }
 
   setPageFilter() {
     switch(this.$routeParams.filterCausa) {
       case 'Saude':
-        this.pageTitle = 'Saúde';
-        this.pageImage = './assets/images/saude/2.png';
+        this.pageTitle='Saúde';
+        this.pageImage='./assets/images/saude/2.png';
+         this.listOngFilterToDisplay()
         break;
-      case 'CombateAProbreza':
-        this.pageTitle = 'Combate a Pobreza';
-        this.pageImage = './assets/images/combate_pobreza/1.png';
+      case 'CombateAProbreza': 
+        this.pageTitle='Combate a Pobreza';
+        this.pageImage='./assets/images/combate_pobreza/1.png';
+        this.listOngFilterToDisplay()
         break;
-      case 'Educação':
-        this.pageTitle = 'Educação';
-        this.pageImage = './assets/images/educacao/lousa_edu3.jpg';
+      case 'Educacao': 
+        this.pageTitle='Educação';
+        this.pageImage='./assets/images/educacao/lousa_edu3.jpg';
+        this.listOngFilterToDisplay()
         break;
-      default:
-      this.pageTitle = 'Ongs que apoiamos';
-      this.pageImage = './assets/images/educacao/lousa_edu3.jpg';
-
+      default: 
+        this.pageTitle='Ongs que apoiamos';
+        this.pageImage='./assets/images/educacao/lousa_edu3.jpg';
+        this.listOngToDisplay = this.listOng;
     }
   }
 
+  listOngFilterToDisplay() {
+    this.listOng.map(ong => {
+      if (ong.areaDeAtuacao === this.$routeParams.filterCausa){
+          this.listOngToDisplay.push(ong);
+      }
+    })
+  }
 
 }
 
-export default angular.module('doebemOrgApp.listOngs', [
-  ngRoute])
-    .config(routing)
-    .component('listOngs', {
-      template: require('./list-ongs.pug'),
-      controller: ListOngsController
-    })
-    .name;
+export default angular.module('doebemOrgApp.listOngs', [ ngRoute]) .config(routing) .component('listOngs', {
+  template: require('./list-ongs.pug'), controller: ListOngsController
+}) 
+.name;
