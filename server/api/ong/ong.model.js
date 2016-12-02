a2a974ea09d5343c834a303ca08be4cd9de7f10e
Jean Philip de Rogatis <jrogatis@iMac-de-Jean.local>
2016-12-02 15:26:54 -0200

    yeahhh

--- server/api/ong/ong.model.js
+++ server/api/ong/ong.model.js
@@ -3,22 +3,22 @@
 import mongoose from 'mongoose';
 
 var OngSchema = new mongoose.Schema({
-      nomeFantasia: String,
-      razaoSocial: String,
-      end: String,
-      cidade: String,
-      estado: String,
-      areaDeAtuacao: String,
-      desc: String,
-      logo: String,
-      localidadesDeAtuacao: String,
-      backgroundImage: String,
-      imagens: [{imagem:String}],
-      anoFundacao: String,
-      analiseGestao: String,
-      analiseTransparencia: String,
-      analiseImpacto: String,
-      linkPdf: String
+  nomeFantasia: String,
+  razaoSocial: String,
+  end: String,
+  cidade: String,
+  estado: String,
+  areaDeAtuacao: String,
+  desc: String,
+  logo: String,
+  localidadesDeAtuacao: String,
+  backgroundImage: String,
+  imagens: [{imagem: String}],
+  anoFundacao: String,
+  analiseGestao: String,
+  analiseTransparencia: String,
+  analiseImpacto: String,
+  linkPdf: String
 });
 
 export default mongoose.model('ong', OngSchema);
