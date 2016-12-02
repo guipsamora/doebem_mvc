62a4a3f05a4887941c97914e69446e97b1843eff
guipsamora <gui.psamora@gmail.com>
2016-12-02 13:07:32 -0200

    Adição de slug no schema

--- server/api/ong/ong.model.js
+++ server/api/ong/ong.model.js
@@ -1,24 +1,25 @@
 'use strict';
 
 import mongoose from 'mongoose';
-
+//saudeCrianca
 var OngSchema = new mongoose.Schema({
-      nomeFantasia: String,
-      razaoSocial: String,
-      end: String,
-      cidade: String,
-      estado: String,
-      areaDeAtuacao: String,
-      desc: String,
-      logo: String,
-      localidadesDeAtuacao: String,
-      backgroundImage: String,
-      imagens: [{imagem:String}],
-      anoFundacao: String,
-      analiseGestao: String,
-      analiseTransparencia: String,
-      analiseImpacto: String,
-      linkPdf: String
+	nomeFantasia: String,	
+	razaoSocial: String,
+	end: String,
+	cidade: String,
+	estado: String,
+	areaDeAtuacao: String,
+	desc: String,
+	logo: String,
+	slug: String,
+	localidadesDeAtuacao: String,
+	backgroundImage: String,
+	imagens: [{imagem:String}],
+	anoFundacao: String,
+	analiseGestao: String,
+	analiseTransparencia: String,
+	analiseImpacto: String,
+	linkPdf: String
 });
 
 export default mongoose.model('ong', OngSchema);
