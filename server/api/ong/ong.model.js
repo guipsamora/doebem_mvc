6925ee2dcfe3fa2c7e8c1062dc94670a6740d242
guipsamora <gui.psamora@gmail.com>
2016-12-05 22:37:41 -0200

    Ajustes na página de cadastro

--- server/api/ong/ong.model.js
+++ server/api/ong/ong.model.js
@@ -3,27 +3,31 @@
 import mongoose from 'mongoose';
 //saudeCrianca
 var OngSchema = new mongoose.Schema({
-	nomeFantasia: String,	
-	razaoSocial: String,
-	cnpj: String,
-	logradouro: String,
-	num: String,
-	cidade: String,
-	estado: String,
-	cep: String,
-	causa: String,
-	desc: String,
-	logo: String,
-	slug: String,
-	localidadesDeAtuacao: String,
-	backgroundImage: String,
-	imagens: [{imagem:String}],
-	anoFundacao: String,
-	analiseGestao: String,
-	analiseTransparencia: String,
-	analiseImpacto: String,
-	linkPdf: String,
-	urlSite: String
+  nomeFantasia: String,
+  razaoSocial: String,
+  cnpj: String,
+  telefone: String,
+  email: String,
+  urlSite: String,
+  logradouro: String,
+  num: String,
+  cidade: String,
+  estado: String,
+  cep: String,
+  causa: String,
+  desc: String,
+  logo: String,
+  slug: String,
+  localidadesDeAtuacao: String,
+  backgroundImage: String,
+  imagens: [{
+    imagem: String
+  }],
+  anoFundacao: String,
+  analiseGestao: String,
+  analiseTransparencia: String,
+  analiseImpacto: String,
+  linkPdf: String
 });
 
 export default mongoose.model('ong', OngSchema);
