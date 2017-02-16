177d94a6f02a12cf1fa041b86b5bc7cdc072949d
Guilherme Samora <gui.psamora@gmail.com>
2017-02-16 09:54:46 -0200

    Criação de outros modelos para base de dados

--- server/api/checkoutForm/checkoutForm.model.js
+++ server/api/checkoutForm/checkoutForm.model.js
@@ -2,12 +2,63 @@
 
 import mongoose from 'mongoose';
 
-var checkoutFormSchema = new mongoose.Schema({
+var donationSchema = new mongoose.Schema({
+  
+  Payment: {
+    // iremos aceitar apenas cartão de crédito
+    // default cartão de crédito para todas as operações
+    Type: { type: 'String', default: 'CreditCard', required: true },
+    Amount: { type: 'Number', required: true },
+    // default número de parcelas ser apenas 1, não iremos parcelar doação né
+    Installments: { type: 'Number', required: true, default: 1 }
+  }
+});
 
-   MerchantOrderId:{
-     type: 'String',
-     default: '2014111703'
-   },
+var authorResponseSchema = new mongoose.Schema({
+
+    MerchantOrderId: { type: 'String', required: true },
+    Customer: {
+        Name: { type: 'String', required: true }
+    },
+    Payment: {
+        Installments: { type: 'Number', required: true },
+        Interest: { type: 'String', required: true },
+        Capture: { type: 'Boolean', required: true },
+        Authenticate: { type: 'Boolean', required: true },
+        CreditCard: {
+          CardNumber: { type: 'String', required: true },
+          Holder: { type: 'String', required: true },
+          ExpirationDate: { type: 'String', required: true },
+          SaveCard: { type: 'Boolean', required: true },
+          Brand: { type: 'String', required: true }
+        },
+        ProofOfSale: { type: 'String', required: true },
+        Tid: { type: 'String', required: true },
+        AuthorizationCode: { type: 'String', required: true },
+        PaymentId: { type: 'String', required: true },
+        Type: { type: 'String', required: true },
+        Amount: { type: 'Number', required: true },
+        Currency: { type: 'String', required: true },
+        Country: { type: 'String', required: true },
+        ExtraDataCollection: [],
+        Status: { type: 'Number', required: true },
+        ReturnCode: { type: 'String', required: true },
+        ReturnMessage: { type: 'String', required: true },
+        // Como fazer esse?
+        Links: [],
+    }
+})
+
+var captureResponseSchema = new mogoose.Schema({
+  
+    "Status": { type: 'Number', required: true },
+    "ReturnCode": { type: 'String', required: true },
+    "ReturnMessage": { type: 'String', required: true },
+    "Links": []
+
+})
+
+var userSchema = new mongoose.Schema({
 
   Customer: {
     Name: {
@@ -27,43 +78,81 @@ var checkoutFormSchema = new mongoose.Schema({
       required: true,
     }
   },
-  Payment: {
-   //  iremos aceitar apenas cartão de crédito
-    Type: {
-      type: 'String',
-      // default cartão de crédito para todas as operações
-      default: 'CreditCard',
-      required: true,
-    },
-    Amount: {
-      type: 'Number',
-      required: true
-    },
-    Installments: {
-      type: 'Number',
-      required: true,
-      // default número de parcelas ser apenas 1, não iremos parcelar doação né
-      default: 1
-    },
-    CreditCard: {
-      CardNumber: {
-        type: 'String',
-        required: true
-      },
-      ExpirationDate: {
-        type: 'String',
-        required: true
-      },
-      SecurityCode: {
-        type: 'String',
-        required: true
-      },
-      Brand: {
-        type: 'String',
-        required: true
-      }
-    }
-  }
+  Transactions: [donationSchema],
+  AuthorizationResponse: [authorResponseSchema],
+  CaptureResponse: [captureResponseSchema],
+
 });
+
+
+
+
+
+// ----------------------------------------------------------------//
+// MODELO DE OBJETO QUE A CIELO PEDE E JÁ TESTADO - APENAS CONSULTA
+// ----------------------------------------------------------------//
+// var checkoutFormSchema = new mongoose.Schema({
+
+//   MerchantOrderId:{
+//      type: 'String',
+//      default: '2014111703'
+//   },
+
+//   Customer: {
+//     Name: {
+//       type: 'String',
+//       required: true,
+//     },
+//     Email: {
+//       type: 'String',
+//       required: true,
+//     },
+//     CPF: {
+//       type: 'String',
+//       required: true,
+//     },
+//     Cidade: {
+//       type: 'String',
+//       required: true,
+//     }
+//   },
+//   Payment: {
+//    //  iremos aceitar apenas cartão de crédito
+//     Type: {
+//       type: 'String',
+//       // default cartão de crédito para todas as operações
+//       default: 'CreditCard',
+//       required: true,
+//     },
+//     Amount: {
+//       type: 'Number',
+//       required: true
+//     },
+//     Installments: {
+//       type: 'Number',
+//       required: true,
+//       // default número de parcelas ser apenas 1, não iremos parcelar doação né
+//       default: 1
+//     },
+//     CreditCard: {
+//       CardNumber: {
+//         type: 'String',
+//         required: true
+//       },
+//       ExpirationDate: {
+//         type: 'String',
+//         required: true
+//       },
+//       SecurityCode: {
+//         type: 'String',
+//         required: true
+//       },
+//       Brand: {
+//         type: 'String',
+//         required: true
+//     }
+//    }
+//   }
+// });
 
 export default mongoose.model('checkoutForm', checkoutFormSchema);
