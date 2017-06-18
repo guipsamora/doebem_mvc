9e15a2da861a7ef753d1c99b7746c0c268dfbe8a
Guilherme Samora <gui.psamora@gmail.com>
2017-06-18 10:52:29 -0300

    melhorias header

--- client/components/navbar/navbar.component.ts
+++ client/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 'use strict';
-/* eslint no-sync: 0 */
+
 const angular = require('angular');
 const ngMaterial = require('angular-material');
 const duScroll = require('angular-scroll');
@@ -7,22 +7,26 @@ const duScroll = require('angular-scroll');
 export class NavbarComponent {
 
   menu = [
-  {
-    'title': 'METODOLOGIA',
-    'link': 'metodologia'
-  }, {
-    'title': 'TRANSPARÊNCIA',
-    'link': 'transparencia'
-  }, {
-    'title': 'SOBRE',
-    'link': 'sobre'
-  }, {
-    'title': 'DÚVIDAS',
-    'link': 'duvidas'
-  }, {
-    'title': 'BLOG',
-    'link': 'https://medium.com/@doebem'
-  }
+    {
+      'title': 'METODOLOGIA',
+      'link': 'metodologia'
+    }, 
+    {
+      'title': 'TRANSPARÊNCIA',
+      'link': 'transparencia'
+    }, 
+    {
+      'title': 'SOBRE',
+      'link': 'sobre'
+    }, 
+    {
+      'title': 'DÚVIDAS',
+      'link': 'duvidas'
+    }, 
+    {
+      'title': 'BLOG',
+      'link': 'https://medium.com/@doebem'
+    }
   ];
 
   $document;
@@ -44,6 +48,10 @@ export class NavbarComponent {
     this.top = 400;
     this.duration = 1500; //milliseconds
     this.$document = $document;
+  }
+
+  showSize(){
+    console.log(window.innerWidth);
   }
 
   isActive(route) {
@@ -70,49 +78,3 @@ export default angular.module('directives.navbar', [ngMaterial, duScroll])
     controller: NavbarComponent
   })
   .name;
-
-// 'use strict';
-// /* eslint no-sync: 0 */
-
-// import angular from 'angular';
-// import angularAria from 'angular-aria';
-// import ngAnimate from 'angular-animate';
-// import angularMaterial from 'angular-material';
-// import angularMessages from 'angular-messages';
-
-// export class NavbarComponent {
-//   constructor($location, $mdSidenav, $animate, $scope, Auth, $timeout) {
-//     'ngInject';
-//     $scope.pageClass = 'pageNavbar';
-//     this.$location = $location;
-//     this.isLoggedIn = Auth.isLoggedInSync;
-//     this.isAdmin = Auth.isAdminSync;
-//     this.getCurrentUser = Auth.getCurrentUserSync;
-//     this.$scope = $scope;
-//     this.$timeout = $timeout;
-//     this.$animate = $animate;
-//     this.enterState = true;
-//     this.myCssVar = 'animated fadeInDown';
-//     this.$timeout(() => {
-//       this.myCssVar = '';
-//     }, 1000);
-//   }
-
-//   isActive(route) {
-//     return route === this.$location.path();
-//   }
-
-//   animate() {
-//     this.myCssVar = 'animated fadeInDown';
-//     this.$timeout(() => {
-//       this.myCssVar = '';
-//     }, 1000);
-//   }
-// }
-
-// export default angular.module(%27directives.navbar%27, [angularAria, ngAnimate, angularMaterial, angularMessages])
-//   .component('navbar', {
-//     template: require('./navbar.pug'),
-//     controller: NavbarComponent
-//   })
-//   .name;
