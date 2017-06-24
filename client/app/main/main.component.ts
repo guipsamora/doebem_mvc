62b468a1330ad8ef232a94b9d4a8b5600df1ff94
Guilherme Samora <gui.psamora@gmail.com>
2017-06-24 20:27:02 -0300

    limpei código main

--- client/app/main/main.component.ts
+++ client/app/main/main.component.ts
@@ -1,6 +1,5 @@
 const angular = require('angular');
 const ngRoute = require('angular-route');
-const jkAngularCarousel = require('../../../node_modules/angular-jk-carousel/dist/jk-carousel.js');
 
 import routing from './main.routes';
 import contactForm from '../../components/contact-form/contact-form.component';
@@ -23,26 +22,7 @@ export class MainController {
     this.$location = $location;
     this.listOng = [];
     this.listOngToDisplay = [];
-
-
-    this.carouselImages = [
-      {src: '../../assets/images/carousel/img_1.jpg'},
-      {src: '../../assets/images/carousel/img_2.jpg'},
-      {src: '../../assets/images/carousel/img_3.jpg'}
-    ];
-
-    this.causas = [
-      {title: 'Educação', subtitle: '(em breve)', image: '../../assets/images/portfolio/education.jpg', link: '/' },
-      {title: 'Saúde', subtitle: '', image: '../../assets/images/portfolio/healthcare.png', link: '/listOngs/saude' },
-      {title: 'Combate à Pobreza', subtitle: '(em breve)', image: '../../assets/images/portfolio/poverty.png', link: '/' }
-    ];
-
     this.stepOptions = [
-      // {
-      //   title: 'Escolha',
-      //   desc: 'A causa que mais se identifica.',
-      //   icon: 'fa-heart-o'
-      // },
       {
         title: 'Selecione',
         desc: 'Uma organização eficiente.',
@@ -58,19 +38,13 @@ export class MainController {
         desc: 'O impacto da sua doação.',
         icon: 'fa-envelope-o'
       }
-      ];
+    ];
   }
 
   carregaLista() {
   this.$http.get(`api/ong/`)
       .then(res => {
         this.listOng = res.data;
-        // console.log(this.listOng);
-        // this.listOngFilterToDisplay();
-        // if (this.$routeParams.filterCausa === undefined &&
-        //     this.listOngToDisplay.length === 0) {
-        //       this.listOngToDisplay = this.listOng;
-        // }
       });
   }
 
@@ -81,7 +55,7 @@ export class MainController {
 }
 
 export default angular.module('doebemOrgApp.main',
- [ngRoute, 'jkAngularCarousel', contactForm])
+ [ngRoute, contactForm])
     .config(routing)
     .component('main', {
       template: require('./main.pug'),
