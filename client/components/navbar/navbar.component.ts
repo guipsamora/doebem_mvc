01f80c4c65a18b0847c8930b55ca58be660f22bd
Guilherme Samora <gui.psamora@gmail.com>
2017-09-14 10:27:02 -0300

    md-dialog funcionando

--- client/components/navbar/navbar.component.ts
+++ client/components/navbar/navbar.component.ts
@@ -4,8 +4,22 @@ const angular = require('angular');
 const ngMaterial = require('angular-material');
 const duScroll = require('angular-scroll');
 
+
 export class NavbarComponent {
 
+  $mdDialog;
+  dialog: Function;
+  $document;
+  $location;
+  $scope;
+  duration: number;
+  top: number;
+  anchorSmoothScroll;
+  isLoggedIn: Function;
+  isAdmin: Function;
+  getCurrentUser: Function;
+  isCollapsed = true;
+  
   menu = [
     {
       'title': 'METODOLOGIA',
@@ -29,17 +43,7 @@ export class NavbarComponent {
     }
   ];
 
-  $document;
-  $location;
-  duration: number;
-  top: number;
-  anchorSmoothScroll;
-  isLoggedIn: Function;
-  isAdmin: Function;
-  getCurrentUser: Function;
-  isCollapsed = true;
-
-  constructor($location, Auth, $document) {
+  constructor($location, Auth, $document, $mdDialog, $scope) {
     'ngInject';
     this.$location = $location;
     this.isLoggedIn = Auth.isLoggedInSync;
@@ -48,6 +52,8 @@ export class NavbarComponent {
     this.top = 400;
     this.duration = 1500; //milliseconds
     this.$document = $document;
+    this.$mdDialog = $mdDialog;
+    this.$scope = $scope;
   }
 
   isActive(route) {
@@ -66,7 +72,41 @@ export class NavbarComponent {
     var section = angular.element(document.getElementById(eID));
     this.$document.scrollToElementAnimated(section, 70, this.duration);
   }
+
+
+  showDialog() {
+    this.dialog = this.$mdDialog.show({
+      scope: this.$scope,
+      preserveScope: true,
+      controller: DialogController,
+      templateUrl: 'dialogDonateNavbar.tmpl.pug',
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      fullscreen: this.$scope.customFullscreen // Only for -xs, -sm breakpoints.
+    });
+  }
+
+
+
 }
+
+
+function DialogController($scope, $mdDialog, $inject) {
+  
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+  
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+  
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+  
+DialogController.$inject = ['$scope', '$mdDialog'];
 
 export default angular.module('directives.navbar', [ngMaterial, duScroll])
   .component('navbar', {
