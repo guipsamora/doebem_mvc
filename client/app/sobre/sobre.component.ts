156776e4a463a48242582d5c57e0c800c63c410f
Guilherme Samora <gui.psamora@gmail.com>
2017-10-05 10:59:24 -0300

    comentar codigo

--- client/app/sobre/sobre.component.ts
+++ client/app/sobre/sobre.component.ts
@@ -37,33 +37,33 @@ export class SobreController {
 
     this.listaPessoas = [
       {
-       nome: 'Elisa',
-       img: './assets/images/team/1.jpg',
-       area: 'Marketing',
-       linkedin: 'https://www.linkedin.com/in/elisa-de-rooij-mansur-30435854/en',
-       bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT. \
-             Elisa construiu sua carreira na indústria de infraestrutura, mas \
-             sempre esteve envolvida com projetos sociais desde a adolescência. \
-             É Yunus&Youth Fellow, Global Shaper e Embaixadora da Draper University no Brasil.'
+        nome: 'Elisa',
+        img: './assets/images/team/1.jpg',
+        area: 'Marketing',
+        linkedin: 'https://www.linkedin.com/in/elisa-de-rooij-mansur-30435854/en',
+        // bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT. \
+        //       Elisa construiu sua carreira na indústria de infraestrutura, mas \
+        //       sempre esteve envolvida com projetos sociais desde a adolescência. \
+        //       É Yunus&Youth Fellow, Global Shaper e Embaixadora da Draper University no Brasil.'
       },
       {
         nome: 'Rhayana',
         img: './assets/images/team/3.jpg',
         area: 'Pesquisa',
         linkedin: 'https://www.linkedin.com/in/rhayana-holz-a1916a5a/',
-        bio: 'Formada em Economia pela Universidade Federal de Ouro Preto e mestre em Economia pela Universidade \
-         Federal de Viçosa. Ao longo dos cinco anos tem se dedicado na área de pesquisa que envolve \
-         métodos quantitativos associados a Avaliação de Impacto Social.'
+        // bio: 'Formada em Economia pela Universidade Federal de Ouro Preto e mestre em Economia pela Universidade \
+        //  Federal de Viçosa. Ao longo dos cinco anos tem se dedicado na área de pesquisa que envolve \
+        //  métodos quantitativos associados a Avaliação de Impacto Social.'
       },
       {
-       nome: 'Guilherme',
-       img: './assets/images/team/2.jpg',
-       area: 'Tecnologia',
-       linkedin: 'https://br.linkedin.com/in/guisamora',
-       bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT, \
-       Elisa construiu sua carreira empresarial na indústria de infraestrutura, mas \
-       sempre esteve envolvida com projetos sociais desde a adolescência. \
-       É Yunus&Youth Fellow, Global Shaper e Embaixadora da Draper University no Brasil.'
+        nome: 'Guilherme',
+        img: './assets/images/team/2.jpg',
+        area: 'Tecnologia',
+        linkedin: 'https://br.linkedin.com/in/guisamora',
+        //  bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT, \
+        //  Elisa construiu sua carreira empresarial na indústria de infraestrutura, mas \
+        //  sempre esteve envolvida com projetos sociais desde a adolescência. \
+        //  É Yunus&Youth Fellow, Global Shaper e Embaixadora da Draper University no Brasil.'
       }
     ];
 
