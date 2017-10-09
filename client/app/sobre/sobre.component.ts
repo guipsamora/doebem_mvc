b48533f170b8651f493944561735af25cedb0f38
Guilherme Samora <gui.psamora@gmail.com>
2017-10-09 10:22:29 -0300

    inclusao bio

--- client/app/sobre/sobre.component.ts
+++ client/app/sobre/sobre.component.ts
@@ -41,29 +41,33 @@ export class SobreController {
         img: './assets/images/team/1.jpg',
         area: 'Marketing',
         linkedin: 'https://www.linkedin.com/in/elisa-de-rooij-mansur-30435854/en',
-        // bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT. \
-        //       Elisa construiu sua carreira na indústria de infraestrutura, mas \
-        //       sempre esteve envolvida com projetos sociais desde a adolescência. \
-        //       É Yunus&Youth Fellow, Global Shaper e Embaixadora da Draper University no Brasil.'
+        email: 'elisa.mansur@doebem.org.br',
+        bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT. \
+              Elisa construiu sua carreira na indústria de infraestrutura, mas \
+              sempre esteve envolvida com projetos sociais desde a adolescência. \
+              É Yunus&Youth Fellow e faz parte da rede Global Shapers.'
       },
       {
         nome: 'Rhayana',
         img: './assets/images/team/3.jpg',
         area: 'Pesquisa',
         linkedin: 'https://www.linkedin.com/in/rhayana-holz-a1916a5a/',
-        // bio: 'Formada em Economia pela Universidade Federal de Ouro Preto e mestre em Economia pela Universidade \
-        //  Federal de Viçosa. Ao longo dos cinco anos tem se dedicado na área de pesquisa que envolve \
-        //  métodos quantitativos associados a Avaliação de Impacto Social.'
+        email: 'rhayana.holz@doebem.org.br',
+        bio: 'Formada em Economia pela Universidade Federal de Ouro Preto e mestre em \
+              Economia pela Universidade Federal de Viçosa. Ao longo dos últimos cinco \
+              anos tem se dedicado na área de pesquisa que envolve \
+              métodos quantitativos associados a Avaliação de Impacto Social.'
       },
       {
         nome: 'Guilherme',
         img: './assets/images/team/2.jpg',
         area: 'Tecnologia',
         linkedin: 'https://br.linkedin.com/in/guisamora',
-        //  bio: 'Formada em Administração pela PUC-Rio e mestranda em negócios no MIT, \
-        //  Elisa construiu sua carreira empresarial na indústria de infraestrutura, mas \
-        //  sempre esteve envolvida com projetos sociais desde a adolescência. \
-        //  É Yunus&Youth Fellow, Global Shaper e Embaixadora da Draper University no Brasil.'
+        email: 'guilherme.samora@doebem.org.br',
+         bio: 'Formado em Administração pelo Mackenzie, participou do curso de Avaliação \
+               Econômica de Projetos Sociais pela Fundação Itau Social. Guilherme construiu \
+               sua carreira sua carreira em empresas de tecnologia e participou da primeira turma \
+               de incubação da Yunus Brasil em 2014.'
       }
     ];
 
