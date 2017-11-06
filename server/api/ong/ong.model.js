280238709ef1be222701cfe8d93cdbdde73ee9fa
Guilherme Samora <gui.psamora@gmail.com>
2017-11-06 10:37:11 -0200

    ajuestes pagOng

--- server/api/ong/ong.model.js
+++ server/api/ong/ong.model.js
@@ -5,133 +5,102 @@ import mongoose from 'mongoose';
 const OngSchema = new mongoose.Schema({
   nomeFantasia: {
     type: 'String',
-    required: true
   },
   razaoSocial: {
     type: 'String',
-    required: true
   },
   cnpj: {
     type: 'String',
-    required: true
   },
   anoFundacao: {
     type: 'String',
-    required: true
   },
   telefone: {
     type: 'String',
-    required: true
   },
   email: {
     type: 'String',
-    required: true
   },
   urlSite: {
     type: 'String',
-    required: true
   },
   slug: {
     type: 'String',
-    required: true
   },
   logradouro: {
     type: 'String',
-    required: true
   },
   num: {
     type: 'String',
-    required: true
   },
   cidade: {
     type: 'String',
-    required: true
   },
   estado: {
     type: 'String',
-    required: true
   },
   cep: {
     type: 'String',
-    required: true
   },
   logo: {
     type: 'String',
-    required: true
   },
   backgroundImage: {
     type: 'String',
-    required: true
   },
   imagens: [{
     imagem: {
       type: 'String',
-      required: true
     }
   }],
   causa: {
     type: 'String',
-    required: true
   },
   causaFrontEnd: {
     type: 'String',
-    required: true
   },
   localidadesDeAtuacao: [{
     localidade: {
       type: 'String',
-      required: true
     }
   }],
   desc: {
     type: 'String',
-    required: true
   },
   sobre: {
     type: 'String',
-    required: true
   },
   analiseGestao: {
     type: 'String',
-    required: true
   },
   analiseTransparencia: {
     type: 'String',
-    required: true
   },
   analiseImpacto: {
     type: 'String',
-    required: true
   },
   linkPdf: {
     type: 'String',
-    required: true
   },
   linkPaypal: {
     type: 'String',
-    required: true
   },
   videoYoutube: {
     type: 'String',
-    required: true
   },
   papers: [{
     name: {
       type: 'String',
-      required: true
     },
     link: {
       type: 'String',
-      required: true
     }
   }],
   atuacao: {
     type: 'String',
-    required: true
   },
   givewell: {
     type: 'Boolean',
-    required: true
   },
   disclaimer: {
     type: 'String',
