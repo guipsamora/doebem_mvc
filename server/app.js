eede7deb73778ff3aca68a5a5a95ac674edfc35d
Guilherme Samora <gui.psamora@gmail.com>
2018-04-18 23:53:07 -0300

    ajuste app.js

--- server/app.js
+++ server/app.js
@@ -28,16 +28,17 @@ var app = express();
 
 app.use(compress());
 
+app.use(require('prerender-node').set('prerenderToken', 'iPohBRFx4DLYiMAKixyK'));
+
 var server = http.createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: config.env !== 'production',
   path: '/socket.io-client'
 });
+
 require('./config/socketio').default(socketio);
 require('./config/express').default(app);
 require('./routes').default(app);
-
-app.use(require('prerender-node').set('prerenderToken', 'iPohBRFx4DLYiMAKixyK'));
 
 // Start server
 function startServer() {
