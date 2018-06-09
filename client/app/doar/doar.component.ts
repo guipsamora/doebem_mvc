057093a1adcd627d4153df5638d29c87ec058879
Guilherme Samora <gui.psamora@gmail.com>
2018-06-09 12:02:15 +0200

    livekey

--- client/app/doar/doar.component.ts
+++ client/app/doar/doar.component.ts
@@ -94,8 +94,8 @@ export class DoarController {
     // INICIAR A INSTÂNCIA DO CHECKOUT
     // declarando um callback de sucesso
     var checkout = new PagarMeCheckout.Checkout({
-      'encryption_key': 'ek_test_QifcmAvy9wCtsgrUr04o884mXp1HiP',
-      // 'encryption_key': 'ek_live_3yykMegYY2XTPClgA1qjui2gSlvVzG',
+      // 'encryption_key': 'ek_test_QifcmAvy9wCtsgrUr04o884mXp1HiP',
+      'encryption_key': 'ek_live_3yykMegYY2XTPClgA1qjui2gSlvVzG',
       success: (data) => {
 
         data.amount = amountValue;
